a9639434766cca7ab0c25f096536c97ff9c442c2
lovettolaedo <loodiakpa@nigerianstat.gov.ng>
2025-07-10 14:15:10 +0100

    Resolved merge conflict in hello_NBS6.py

--- first_script.py
+++
@@ -1,3 +0,0 @@
-print("Hello Nigeria!")
-print("Hello Nigeria from Siyudi!")
-print("Hello Nigeria from Tekiyat!")
--- hello_NBS6.py
+++
@@ -1,3 +0,0 @@
-print("Dear Colleagues! This is my first Python script.")
-country = "Cape Verde"
-print(f"Hello, {country}! Welcome to your first Python project.")
