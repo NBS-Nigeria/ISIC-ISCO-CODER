1d91d89a7ad7fff5d47b31e788c20d72db56e875
SalihijoI <bubasalihijo@gmail.com>
2025-07-16 22:59:06 +0100

    Week3 Lab

--- hello_NBS.py
+++ hello_NBS.py
@@ -1,2 +1,34 @@
 country = "Canada"
 print(f"Hello, {country}! Welcome to your first Python project.")
+
+#WEEK 3
+proffession = "Doctor"
+networth = 2300000.02
+children = 4
+is_retired = False
+
+print (type(proffession))
+print (type(networth))
+print (type(children))
+print (type(is_retired))
+
+Nigerian_state = ["Taraba", "Adamawa", "Benue", "Plateau", "Lagos"]
+print ("")
+print ("Five Nigerian States:")
+print (Nigerian_state)
+
+Currency_code = ("Euro", "Poounds", "Naira")
+print ("")
+print ("Three currency codes")
+print (Currency_code)
+
+Student_ID = {"1111", "2222", "1111", "3333"}
+print ("")
+print ("Student ID's")
+print (Student_ID)
+
+
+Scores = {"Ahmed":96, "Susan":52, "Jibril":40}
+print ("")
+print ("Results")
+print (Scores)
