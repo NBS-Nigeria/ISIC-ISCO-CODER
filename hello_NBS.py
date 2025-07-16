fd98afc6438e89d2a6019c584b6a9f3c2ae66751
liz-khalifar <lizkhalifar@gmail.com>
2025-07-16 14:33:26 +0100

    feat: add print state >> ments to hello_NBS.py

--- hello_NBS.py
+++ hello_NBS.py
@@ -1,1 +1,26 @@
 print("Dear colleagues! this is my first python script")
+country = "Nigeria"
+print(f"Hello, {country}! Welcome to your first Python project.")
+
+state = ["Lagos", "Oyo", "Osun", "Ogun", "Ekiti"]
+print(state[0])        
+print(state[-1])  
+print(state[-2])  
+print(state[-3])      
+print(state[1:5])      
+print(state)
+
+currency = (1600, 2100, 216)              
+print("USD", currency[0])
+print("GBP", currency[1])
+print("RMB", currency[2])
+
+studentIDs = {"ST20577", "ST2063", "ST27362"}
+print(languages)  
+
+scores = {
+    "Liz": 100,
+    "Victoria": 98,
+    "KLoveth": 79
+}
+print(scores)
