27baa46e52973ad6988077890ebd6343573be161
lovettolaedo <loodiakpa@nigerianstat.gov.ng>
2025-07-16 13:50:04 +0100

    Lab Week 3: Practice with data types and structures

--- labs/week-1/hello_NBS6.py
+++ labs/week-1/hello_NBS6.py
@@ -1,1 +1,32 @@
 print("Dear Colleagues! This is my first Python script.")
+
+##################################################################################################
+# Creating variables of different types
+state_count = 36          # Integer
+inflation_rate = 18.2     # Float
+president = "Tinubu"      # String
+has_elections = True      # Boolean
+
+# List of five Nigerian states accross the different regions
+nigerian_states = ["Lagos", "Kano", "Enugu", "Rivers", "Kaduna", "Yobe"]
+
+# Tuple with three currency codes
+currency_codes = ("NGN", "USD", "EUR")
+
+# Set of unique student IDs
+student_ids = {10101, 10102, 10103, 10104, 10105}
+
+# Dictionary mapping student names to scores
+student_scores = {
+    "Ada": 88,
+    "Bola": 74,
+    "Chuka": 91,
+    "Funmi": 67,
+    "Yakubu": 82
+}
+
+# Output all data for verification
+print("Nigerian States:", nigerian_states)
+print("Currency Codes:", currency_codes)
+print("Student IDs:", student_ids)
+print("Student Scores:", student_scores)
